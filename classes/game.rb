e20bc76f7c66cec9c5b6a489c6498c4c53a114f7
Young-Gaius <youngehu8@gmail.com>
2023-10-26 11:02:02 +0100

    modify game.rb file

--- classes/game.rb
+++ classes/game.rb
@@ -1,18 +1,40 @@
-class Item
-    attr_accessor :archived
-    attr_reader :publish_date
-  
-    def initialize(publish_date = Date.today)
-      @publish_date = publish_date
-      @archived = false
-    end
-  
-    def can_be_archived?
-      years_since_publish = (Date.today - publish_date).to_i / 365
-      years_since_publish >= 10
-    end
-  
-    def move_to_archive
-      @archived = can_be_archived?
-    end
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :author
+
+  def initialize(publish_date, multiplayer, last_played_at, id: 0)
+    super(publish_date, id: id)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
+
+  def can_be_archived?
+    super && (Date.today - Date.parse(@last_played_at)).to_i > 365 * 2
+  end
+
+  # def to_json(*_args)
+  #   {
+  #     'id' => @id,
+  #     'genre' => @genre,
+  #     'author' => @author,
+  #     'source' => @source,
+  #     'label' => @label,
+  #     'publish_date' => @publish_date,
+  #     'archived' => @archived,
+  #     'multiplayer' => @multiplayer,
+  #     'last_played_at' => @last_played_at
+  #   }
+  # end
+
+  def to_json(*_args)
+    {
+      id: @id,
+      publish_date: @publish_date.to_s,
+      multiplayer: @multiplayer,
+      last_played_at: @last_played_at.to_s,
+      author: @author.map(&:to_json),
+      label: @label.map(&:to_json)
+    }
+  end
+end
