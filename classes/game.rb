a4bcd7e0f21aaadeef61fc98f4739f240abee9a6
Young-Gaius <youngehu8@gmail.com>
2023-10-26 10:59:21 +0100

    install bundle

--- classes/game.rb
+++ classes/game.rb
@@ -1,27 +1,18 @@
-require_relative 'item'
-
-class Game < Item
-  attr_accessor :multiplayer, :last_played_at, :title
-  attr_reader :author
-
-  def initialize(title, published_date, multiplayer, last_played_at = nil)
-    super('GAME', published_date)
-
-    @title = title
-    @multiplayer = multiplayer
-    @last_played_at = last_played_at unless last_played_at.nil?
-    @author = nil
-  end
-
-  private
-
-  def can_be_archived?
-    unless last_played_at.nil?
-      age_in_seconds = Time.now - last_played_at
-
-      return age_in_seconds > (2 * 365 * 24 * 60 * 60)
+class Item
+    attr_accessor :archived
+    attr_reader :publish_date
+  
+    def initialize(publish_date = Date.today)
+      @publish_date = publish_date
+      @archived = false
     end
-
-    false
+  
+    def can_be_archived?
+      years_since_publish = (Date.today - publish_date).to_i / 365
+      years_since_publish >= 10
+    end
+  
+    def move_to_archive
+      @archived = can_be_archived?
+    end
   end
-end
