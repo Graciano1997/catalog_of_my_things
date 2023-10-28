8ff92d53dfb29cc8b9b2b66b0519d77cff5318b1
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-28 14:05:22 +0100

    update genre_spec tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -20,41 +20,18 @@ describe Author do
       expect(author_object).to respond_to(:id)
     end
 
-    # it 'Should have @items should be Empty  ' do
-    #   title = 'Microverse'
-    #   color = 'Purple'
-    #   label_object = Label.new(title,color)
-    #   expect(label_object.items.length).to be 0
-    # end
+    it ' @items should be Empty  ' do
+      first_name = 'Kirty'
+      last_name = 'Sama'
+      author_object = Author.new(first_name, last_name)
+      expect(author_object.items.length).to be 0
+    end
 
-    # it 'Title Should Match Microverse ' do
-    #   title = 'Microverse'
-    #   color = 'Purple'
-    #   label_object = Label.new(title,color)
-    #   expect(label_object.title).to match /Microverse/i
-    # end
-
-    # it 'Color Should Match Purple ' do
-    #   title = 'Microverse'
-    #   color = 'Purple'
-    #   label_object = Label.new(title,color)
-    #   expect(label_object.color).to match /Purple/i
-    # end
-
-    # it 'id Should not be nil' do
-    #   title = 'Microverse'
-    #   color = 'Purple'
-    #   label_object = Label.new(title,color)
-    #   expect(label_object.id).not_to be nil
-    # end
-
-    # it 'items Should have one book element' do
-    #   title = 'Microverse'
-    #   color = 'Purple'
-    #   label_object = Label.new(title,color)
-    #   book_item=Book.new("Graciano","Good","2023-12-12")
-    #   label_object.add_item(book_item)
-    #   expect(label_object.items.length).to be 1
-    # end
+    it ' Should match the Kirty name  ' do
+      first_name = 'Kirty'
+      last_name = 'Sama'
+      author_object = Author.new(first_name, last_name)
+      expect(author_object.first_name).to match(/Kirty/i)
+    end
   end
 end
